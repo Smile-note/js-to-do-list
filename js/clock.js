ce7afb3f4fa1d4c501eeae8da45b416652e9c3c6
tn2003wl <jsung16@naver.com>
2022-02-09 17:25:02 +0900

    ampm

--- js/clock.js
+++ js/clock.js
@@ -2,10 +2,14 @@ const clock = document.querySelector("h2#clock");
 
 function getClock() {
   const date = new Date();
-  const hours = String(date.getHours()).padStart(2, "0");
+  let hours = date.getHours();
   const minutes = String(date.getMinutes()).padStart(2, "0");
   const seconds = String(date.getSeconds()).padStart(2, "0");
-  clock.innerText = `${hours}:${minutes}:${seconds}`;
+  const ampm = hours >= 12 ? "pm" : "am";
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+
+  clock.innerText = `${hours}:${minutes} ${ampm}`;
 }
 
 getClock();
